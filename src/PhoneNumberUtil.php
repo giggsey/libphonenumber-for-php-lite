19c0665a1bf39003a2c8e6d4fc209172d8b376d6
Joshua Gigg <giggsey@gmail.com>
2024-07-01 12:33:37 +0100

    Area code information lost for all MX numbers in last metadata update
    Mirrors https://github.com/google/libphonenumber/pull/3535

--- src/PhoneNumberUtil.php
+++ src/PhoneNumberUtil.php
@@ -170,6 +170,13 @@ class PhoneNumberUtil
     ];
 
     /**
+     * Set of country codes that doesn't have national prefix, but it has area codes.
+     */
+    protected const COUNTRIES_WITHOUT_NATIONAL_PREFIX_WITH_AREA_CODES = [
+        52, // Mexico
+    ];
+
+    /**
      * Set of country calling codes that have geographically assigned mobile numbers. This may not be
      * complete; we add calling codes case by case, as we find geographical mobile numbers or hear
      * from user reports. Note that countries like the US, where we can't distinguish between
@@ -787,20 +794,25 @@ class PhoneNumberUtil
         if ($metadata === null) {
             return 0;
         }
+
+        $countryCallingCode = $number->getCountryCode();
+
+
         // If a country doesn't use a national prefix, and this number doesn't have an Italian leading
         // zero, we assume it is a closed dialling plan with no area codes.
-        if (!$metadata->hasNationalPrefix() && !$number->isItalianLeadingZero()) {
+        // Note:this is our general assumption, but there are exceptions which are tracked in
+        // COUNTRIES_WITHOUT_NATIONAL_PREFIX_WITH_AREA_CODES.
+        if (!$metadata->hasNationalPrefix() && !$number->isItalianLeadingZero() && !in_array($countryCallingCode, static::COUNTRIES_WITHOUT_NATIONAL_PREFIX_WITH_AREA_CODES)) {
             return 0;
         }
 
         $type = $this->getNumberType($number);
-        $countryCallingCode = $number->getCountryCode();
 
         if ($type === PhoneNumberType::MOBILE
             // Note this is a rough heuristic; it doesn't cover Indonesia well, for example, where area
             // codes are present for some mobile phones but not for others. We have no better way of
             // representing this in the metadata at this point.
-            && in_array($countryCallingCode, self::GEO_MOBILE_COUNTRIES_WITHOUT_MOBILE_AREA_CODES)
+            && in_array($countryCallingCode, static::GEO_MOBILE_COUNTRIES_WITHOUT_MOBILE_AREA_CODES)
         ) {
             return 0;
         }
